38688d092fb9e92fdf745ed33ce70d059662226c
bigyan-bhandari1 <bigyan.bhandari@varosatech.com>
2025-11-19 09:56:36 +0545

    custom form

---
+++ src/pages/task/components/form/useCustomForm.ts
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+interface Props {
+  initialValues: Record<string, string>;
+}
+
+export const useCustomForm = ({ initialValues = {} }: Props) => {
+  const [formData, setFormData] = useState(initialValues);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  const reset = () => {
+    setFormData(initialValues);
+  };
+
+  const register = (name: string) => ({
+    name,
+    value: formData[name] || '',
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
+      setFormData((prev) => ({
+        ...prev,
+        [name]: e.target.value,
+      }));
+    },
+  });
+
+  return {
+    formData,
+    setFormData,
+    register,
+    handleSubmit,
+    reset,
+  };
+};
--- src/pages/task/task-page.tsx
+++ src/pages/task/task-page.tsx
@@ -1,54 +1,35 @@
-import { SearchFilterSection } from './components/search-filter';
-import { TaskCard } from './components/task-card';
-import { TaskModal } from './components/task-modal';
-import { useTaskPage } from './hooks/use-task-page';
+import { useCustomForm } from './components/form/useCustomForm';
 
 export default function TaskPage() {
-  const {
-    isOpen,
-    onClose,
-    handleAddTask,
-    formHooks,
-    formSubmit,
-    onDelete,
-    onEdit,
-    taskId,
-    data,
-    onChangeFilter,
-    filter
+  // const {
+  //   isOpen,
+  //   onClose,
+  //   handleAddTask,
+  //   formHooks,
+  //   formSubmit,
+  //   onDelete,
+  //   onEdit,
+  //   taskId,
+  //   data,
+  //   onChangeFilter,
+  //   filter
 
-  } = useTaskPage();
+  // } = useTaskPage();
+
+  const { formData, register, handleSubmit, reset } = useCustomForm({
+    initialValues: { fullName: '' },
+  });
+
   return (
-    <div className='space-y-5'>
-      <section>
-        <div className='text-3xl font-semibold'>Task Tracker</div>
-        <div>
-          Stay organized and manage your daily tasks effortlessly. Add, edit,
-          delete, and track your to-dos with sorting, filtering, and search —
-          all in one simple interface.
-        </div>
-      </section>
-      <SearchFilterSection onAddTask={handleAddTask} onChangeFilter={onChangeFilter} filter={filter} />
-      <div className='bg-white  border-gray-400 rounded-md p-4 hover:shadow-md transition-shadow h-[60vh] overflow-y-scroll'>
-        {data?.length ? data?.map((item) => (
-          <TaskCard
-            key={item.id}
-            data={item}
-            onDelete={onDelete}
-            onEdit={onEdit}
-          />
-        )) : <div className='grid place-content-center h-full'>
-          <div className='text-4xl'>No Data...</div>
-          <div className='text-lg'>please add Task</div>
-        </div>}
+    <form onSubmit={handleSubmit}>
+      <input {...register('fullName')} placeholder='Name' />
+
+      <div className='flex gap-4'>
+        <button type='submit'>Submit</button>
+        <button type='button' onClick={reset}>
+          Cancel
+        </button>
       </div>
-      <TaskModal
-        isOpen={isOpen}
-        onClose={onClose}
-        formHooks={formHooks}
-        formSubmit={formSubmit}
-        taskId={taskId}
-      />
-    </div>
+    </form>
   );
 }
